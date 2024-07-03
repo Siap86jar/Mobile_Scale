09d697ee8feab0db120562f1e505a261f9f25733
App <refajar86@gmail.com>
2024-07-03 16:29:02 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Batam\\flutter_windows_3.16.9-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Batam\\flutter_windows_3.16.9-stable\\flutter\\bin\\pbl_lulu\\pbl_lulu" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Batam\\flutter_windows_3.16.9-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Batam\\flutter_windows_3.16.9-stable\\flutter\\bin\\pbl_lulu\\pbl_lulu"
+  "FLUTTER_ROOT=C:\\Users\\Batam\\flutter_windows_3.16.9-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Batam\\flutter_windows_3.16.9-stable\\flutter\\bin\\pbl_lulu\\pbl_lulu\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Batam\\flutter_windows_3.16.9-stable\\flutter\\bin\\pbl_lulu\\pbl_lulu"
+  "FLUTTER_TARGET=C:\\Users\\Batam\\flutter_windows_3.16.9-stable\\flutter\\bin\\pbl_lulu\\pbl_lulu\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNDBlOTc2YmVkZmY1N2U2OWUxYjNkODlhN2MyYTNjNjE3YTAzZGFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Batam\\flutter_windows_3.16.9-stable\\flutter\\bin\\pbl_lulu\\pbl_lulu\\.dart_tool\\package_config.json"
+)
